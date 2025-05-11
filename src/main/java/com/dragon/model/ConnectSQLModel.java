b70bb63a873f1b7addd876c88642b347d22e708b
NGUYEN HA <hoanghalamthao@gamil.com>
2025-05-11 21:13:04 +0700

    init file

---
+++ src/main/java/com/dragon/model/ConnectSQLModel.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dragon.model;
+
+/**
+ *
+ * @author HOANGHA
+ */
+public class ConnectSQLModel {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString(); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+    
+}
---
+++ src/main/webapp/views/home.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : home
+    Created on : Mar 1, 2025, 10:32:23 PM
+    Author     : HOANGHA
+--%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>CONNECT DATABASE</title>
+    </head>
+    <body>
+        <h1>${name}</h1>
+        <h1>${notification}</h1>
+    </body>
+</html>
